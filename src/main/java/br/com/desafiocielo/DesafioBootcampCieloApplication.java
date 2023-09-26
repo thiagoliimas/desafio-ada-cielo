c831d8039fae428e30182ef2bbdb07b7415b8f4f
ThiagoLima <thiagoliimas@outlook.com>
2023-09-26 01:00:20 -0300

    infra classes

---
+++ src/main/java/br/com/desafiocielo/DesafioBootcampCieloApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiocielo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBootcampCieloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBootcampCieloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafiocielo/DesafioBootcampCieloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiocielo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBootcampCieloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
